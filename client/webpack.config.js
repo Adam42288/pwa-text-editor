ae3c98757cda5cfa30c42fbd125b4c41cea0c8a4
Adam <asantillana@apple.com>
2023-03-14 18:46:17 -0500

    updated webpack to fix icon src path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,75 +1,76 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
       // webpack plugin to generate an HTML5 file for you that includes all your webpack bundles in the body using script tags.
       new HtmlWebpackPlugin({
-        title: 'JATE',
-        template: './index.html',
+        title: "JATE",
+        template: "./index.html",
       }),
-    // webpack plugin to generate a manifest file for your PWA.
-    new InjectManifest({
-      swSrc: './src-sw.js',
-      swDest: 'src-sw.js',
+      // webpack plugin to generate a manifest file for your PWA.
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
 
-    new WebpackPwaManifest({
-      fingerprints: false,
-      inject: true,
-      name: 'JATE',
-      short_name: 'JATE',
-      description: 'JATE',
-      background_color: '#01579b',
-      theme_color: '#ffffff',
-      start_url: '/',
-      publicPath: '/',
-      icons: [
-        {
-          src: path.resolve('src/images/icons/icon-512x512.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-        },
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "JATE",
+        short_name: "JATE",
+        description: "JATE",
+        background_color: "#01579b",
+        theme_color: "#ffffff",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
-    }),
-  ],
 
-  // TODO: Add CSS loaders and babel to webpack.
-
+    // TODO: Add CSS loaders and babel to webpack.
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
         },
-      },
-    },
- ],
+      ],
     },
   };
 };
