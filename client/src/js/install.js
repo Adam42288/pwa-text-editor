773c9f4851d3504ca4e2710dd3551cffc0041a42
adam <asantillana@apple.com>
2023-03-14 14:55:56 -0500

    added code for install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,36 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    window.deferredPrompt = event;
+
+    // Show the `butInstall` element
+    butInstall.classList.toggle('hidden', false);
+
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+
+    // Show the install prompt
+    promptEvent.prompt();
+
+    // Reset the deferred prompt variable
+    window.deferredPrompt = null;
+
+    // Hide the install button
+    butInstall.classList.toggle('hidden', true);
+
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('appinstalled fired', event);
+    window.deferredPrompt = null;
+});
